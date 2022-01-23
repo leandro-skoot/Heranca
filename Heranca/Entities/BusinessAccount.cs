f44999f58b77f157d229d6dbcac0a5d4adc0793d
Leandro Silva <leandro.skoot@gmail.com>
2022-01-23 12:35:32 -0300

    Exercicios de Heranca!

---
+++ Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace Heranca.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoandLimit { get; set; }
+
+        public BusinessAccount() { }
+
+        public BusinessAccount(int number, string holder, double balance, double loandLimit) : base(number, holder, balance)
+        {
+            LoandLimit = loandLimit;
+        }
+
+        public void Loan(double amont)
+        {
+            if(amont <= LoandLimit)
+            {
+                Balance += amont;
+            }
+        }
+    }
+}
